830212641b2161acc0148ad6bce2081f8aa0da72
enhuush_enhmunh <enhuush0704@gmail.com>
2021-03-23 10:38:51 +0800

    parallax

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import './App.scss'
+const App = () => {
+  return (
+    <>  
+      <div className="pimg1">
+        <div className="ptext">
+          <span className="border">
+            Parallax Website
+          </span>
+        </div>
+      </div> 
+      <section className="section section-light">
+        <h2>Section One</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer lacus turpis, pellentesque quis bibendum in, tincidunt ornare sem. Interdum et malesuada fames ac ante ipsum primis in faucibus. Etiam tempus dictum magna. Etiam tincidunt vulputate est. In hac habitasse platea dictumst. Nulla ornare turpis eget metus sagittis, vulputate feugiat tellus accumsan. Quisque sodales ligula eget tellus egestas semper. Sed vestibulum eros a purus pulvinar, quis cursus tellus ornare. Morbi metus purus, molestie ut molestie nec, finibus non augue.
+        </p>
+      </section>
+      <div className="pimg2">
+        <div className="ptext">
+          <span className="border">
+            Image Two Text
+          </span>
+        </div>
+      </div> 
+      <section className="section section-dark">
+        <h2>Section Two</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer lacus turpis, pellentesque quis bibendum in, tincidunt ornare sem. Interdum et malesuada fames ac ante ipsum primis in faucibus. Etiam tempus dictum magna. Etiam tincidunt vulputate est. In hac habitasse platea dictumst. Nulla ornare turpis eget metus sagittis, vulputate feugiat tellus accumsan. Quisque sodales ligula eget tellus egestas semper. Sed vestibulum eros a purus pulvinar, quis cursus tellus ornare. Morbi metus purus, molestie ut molestie nec, finibus non augue.
+        </p>
+      </section>
+      <div className="pimg3">
+        <div className="ptext">
+          <span className="border">
+            Image Three Text
+          </span>
+        </div>
+      </div> 
+      <section className="section section-dark">
+        <h2>Section Three</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer lacus turpis, pellentesque quis bibendum in, tincidunt ornare sem. Interdum et malesuada fames ac ante ipsum primis in faucibus. Etiam tempus dictum magna. Etiam tincidunt vulputate est. In hac habitasse platea dictumst. Nulla ornare turpis eget metus sagittis, vulputate feugiat tellus accumsan. Quisque sodales ligula eget tellus egestas semper. Sed vestibulum eros a purus pulvinar, quis cursus tellus ornare. Morbi metus purus, molestie ut molestie nec, finibus non augue.
+        </p>
+      </section>
+      <div className="pimg1">
+        <div className="ptext">
+          <span className="border">
+            Parallax Website
+          </span>
+        </div>
+      </div> 
+    </>
+  );
+}
+export default App;
